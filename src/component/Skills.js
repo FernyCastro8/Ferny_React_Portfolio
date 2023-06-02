48b542c8a121ad49a91d123be23088fbbf5d1dd3
Ferny <fernc28@outlook.com>
2023-06-01 20:50:53 -0400

    Skill component fin

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -19,71 +19,82 @@ function Skills() {
     const skills = [
         {
             id: 1,
-            name: 'HTML',
-            logo: HTML_logo
+            title: 'HTML',
+            src: HTML_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 2,
-            name: 'CSS',
-            logo: CSS_logo
+            title: 'CSS',
+            src: CSS_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 3,
-            name: 'JavaScript',
-            logo: JavaScript_logo
+            title: 'JavaScript',
+            src: JavaScript_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 4,
-            name: 'Nodejs',
-            logo: Nodejs_logo
+            title: 'Nodejs',
+            src: Nodejs_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 5,
-            name: 'React js',
-            logo: Reactjs_logo
+            title: 'React js',
+            src: Reactjs_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 6,
-            name: 'MongoDB',
-            logo: Mongo_logo
+            title: 'MongoDB',
+            src: Mongo_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 7,
-            name: 'MongoDB',
-            logo: HTML_logo
+            title: 'MongoDB',
+            src: HTML_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 8,
-            name: 'Tailwind CSS',
-            logo: Tailwind_logo
+            title: 'Tailwind CSS',
+            src: Tailwind_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 9,
-            name: 'Terminal',
-            logo: Terminal_logo
+            title: 'Terminal',
+            src: Terminal_logo,
+            style: 'shadow-gray-500'
         },
         {
             id: 10,
-            name: 'Github',
-            logo: Githug_logo
+            title: 'Github',
+            src: Githug_logo,
+            style: 'shadow-gray-500'
         },
     ]
 
     return (
-        <div name='skills' className="w-full h-screen bg-black bg-gradient-to-b from-gray-800 to-black" >
+        <div title='skills'
+            className="w-full bg-black" >
 
-            <div className="max-w-screen mx-auto w-full h-full flex flex-col justify-center p-4 text-white">
+            <div className="max-w-screen-lg mx-auto w-full  flex flex-col justify-center p-4 text-white">
 
                 <div>
                     <p className="text-4xl font-signature  border-b-4 border-gray-500 inline">Skills</p>
-                    <p className="py-6">Techlogies I've worked with</p>
+                    <p className="text-gray-300 py-6">Techlogies I've worked with</p>
                 </div>
 
-                <div className="w-fulsl grid grid-cols-2  ms:grid-col-3 gap-8 text-center py-8 ms:px-0 pt-2">
-                    {skills.map(({ id, name, logo }) => (
-                        <div className="shadow-md hover:scale-105 duration-500 py-2 rounded-lg">
-                            <img key={id} src={logo} alt="" className="w-20 mx-auto" />
-                            <p className="mt-4">{name}</p>
+                <div className="w-full grid grid-cols-2  ms:grid-col-3 gap-8 text-center py-8 px-12 ms:px-0 pt-2">
+                    {skills.map(({ id, title, src, style }) => (
+                        <div className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                            <img key={id} src={src} alt="" className="w-20 mx-auto" />
+                            <p className="mt-4">{title}</p>
                         </div>
                     ))}
                 </div>
